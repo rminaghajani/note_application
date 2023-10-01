e38118952ce9597ee17d8cc399dd8533413a26ab
Armin Aghajani <rminaghajani@gmail.com>
2023-10-02 01:12:37 +0330

    add FAB to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:msh_checkbox/msh_checkbox.dart';
 import 'package:note/data/constants/constants.dart';
+import 'package:note/data/models/task.dart';
+import 'package:note/screens/add_screen.dart';
+import 'package:note/widgets/task_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,130 +16,65 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool isChecked = false;
+  final taskBox = Hive.box<Task>('TaskBox');
+  bool isFabVisible = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bgrcColor,
-      body: Center(
-        child: getTaskItem(),
-      ),
-    );
-  }
-
-  Container getTaskItem() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 24),
-      height: 132,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: taskBoxColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(12),
-        child: getMainContent(),
-      ),
-    );
-  }
-
-  Row getMainContent() {
-    return Row(
-      children: [
-        Image.asset('images/workout.png'),
-        SizedBox(
-          width: 20,
-        ),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Workout with friends'),
-                  MSHCheckbox(
-                    size: 22,
-                    value: isChecked,
-                    colorConfig: MSHColorConfig.fromCheckedUncheckedDisabled(
-                      checkedColor: green,
-                    ),
-                    style: MSHCheckboxStyle.fillScaleCheck,
-                    onChanged: (selected) {
-                      setState(() {
-                        isChecked = selected;
-                      });
-                    },
-                  ),
-                ],
+      floatingActionButton: Visibility(
+        visible: isFabVisible,
+        child: FloatingActionButton(
+          backgroundColor: green,
+          child: Image.asset('images/icon_add.png'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddScreen(),
               ),
-              Text('Second text'),
-              Spacer(),
-              getTimeAndEditBadgs()
-            ],
-          ),
+            );
+          },
         ),
-      ],
+      ),
+      body: SafeArea(
+        child: Center(
+            child: ValueListenableBuilder(
+                valueListenable: taskBox.listenable(),
+                builder: (context, value, child) {
+                  return NotificationListener<UserScrollNotification>(
+                    onNotification: (notif) {
+                      setState(() {
+                        if (notif.direction == ScrollDirection.forward) {
+                          isFabVisible = true;
+                        } else if (notif.direction == ScrollDirection.reverse) {
+                          isFabVisible = false;
+                        }
+                      });
+                      return true;
+                    },
+                    child: ListView.builder(
+                      itemCount: taskBox.values.length,
+                      itemBuilder: (context, index) {
+                        return getTaskItem(taskBox.values.toList()[index]);
+                      },
+                    ),
+                  );
+                })),
+      ),
     );
   }
 
-  Row getTimeAndEditBadgs() {
-    return Row(
-      children: [
-        Container(
-          width: 90,
-          height: 28,
-          decoration: BoxDecoration(
-            color: green,
-            borderRadius: BorderRadius.circular(18),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '10:30',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Image.asset('images/icon_time.png'),
-              ],
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Container(
-          width: 90,
-          height: 28,
-          decoration: BoxDecoration(
-            color: lightGreen,
-            borderRadius: BorderRadius.circular(18),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Edit',
-                  style: TextStyle(color: green),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Image.asset('images/icon_edit.png'),
-              ],
-            ),
-          ),
-        ),
-      ],
+  Widget getTaskItem(Task task) {
+    return Dismissible(
+      key: UniqueKey(),
+      onDismissed: (direction) {
+        task.delete();
+      },
+      child: TaskWidget(
+        task: task,
+      ),
     );
   }
 }
