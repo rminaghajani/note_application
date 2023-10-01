c1e55914272b9ef47cd82b8e196c4ce449c14a4a
Armin Aghajani <rminaghajani@gmail.com>
2023-10-02 01:11:14 +0330

    register adapters for hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:note/screens/add_screen.dart';
 import 'package:note/screens/home_screen.dart';
 
-void main() {
+import 'data/models/task.dart';
+import 'data/models/task_type.dart';
+import 'data/models/task_type_enum.dart';
+
+void main() async {
+  // hive part and registering custom types
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskTypeStatusAdapter());
+  Hive.registerAdapter(TaskTypeAdapter());
+  Hive.registerAdapter(TaskAdapter());
+  await Hive.openBox<Task>('TaskBox');
+
   runApp(Application());
 }
 
@@ -11,6 +25,7 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Varela',
       ),
