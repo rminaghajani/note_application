61a01304198961bc147e95cff6647e67d28ccc83
Armin Aghajani <rminaghajani@gmail.com>
2023-10-02 01:11:23 +0330

    new color added

--- lib/data/constants/constants.dart
+++ lib/data/constants/constants.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 const Color bgrcColor = Color(0xffE5E5E5);
 const Color taskBoxColor = Color(0xffffffff);
 const Color green = Color(0xff18DAA3);
+const Color gray = Color(0xffC5C5C5);
 const Color lightGreen = Color(0xffE2F6F1);
